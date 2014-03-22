8229ec0555b422194bf1b634d4b2a6c6e5b3c550
Tanya <tetyana.sotnikova@gmail.com>
2014-03-22 22:09:09 +0100

    Added layout with different views

--- app/view/Main.js
+++ app/view/Main.js
@@ -3,46 +3,92 @@ Ext.define('HelloWorld.view.Main', {
     xtype: 'main',
     requires: [
         'Ext.TitleBar',
-        'Ext.Video'
+        'Ext.Video',
+        'Ext.List',
+        'Ext.data.Store'
     ],
     config: {
         tabBarPosition: 'bottom',
 
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
+        items: 
+        	[
+         	{
+        	 	title: 'First Tab',
+        	 	iconCls: 'home',
+                layout: 'fit',
+                items:[
+                {
+                	docked: 'top',
                     xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
+                    title: 'First Tab'
                 },
+                {
+            		xtype: 'list',
+                	width: null,
+                	height: null,
+                	store: {
+                        fields: ['name'],
+                        data: [
+                            {name: 'Bicycle'},
+                            {name: 'Car'},
+                            {name: 'Motorbicycle'},
+                            {name: 'Airplane'}
+                        ]
+                    },
 
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+                    itemTpl: '{name}'
+            	}
+                ]
+        	},
+            {
+                title: 'Second Tab',
+                iconCls: 'info',
+                layout: 'fit',
+                items: [
+                {
+                	docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Second Tab'
+                },
+                {
+                	xtype: 'carousel',
+                	width: null,
+                	height: null,
+                	items: [
+                	        {
+                	            html : 'Item 1',
+                	            style: 'background-color: #5E99CC'
+                	        },
+                	        {
+                	            html : 'Item 2',
+                	            style: 'background-color: #759E60'
+                	        },
+                	        {
+                	            html : 'Item 3'
+                	        }
+                	    ]
+                }
+                ]
+                
             },
             {
-                title: 'Get Started',
-                iconCls: 'action',
-
+                title: 'Third Tab',
+                iconCls: 'favorites',
+                layout: 'fit',
                 items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
+                        {
+                        	docked: 'top',
+                            xtype: 'titlebar',
+                            title: 'Third Tab'
+                        },
+                        {
+                        	width: 100,
+                        	height: 20,
+                        	xtype: 'button',
+                        	flex: 1,
+                        	margin: 10,
+                        	text: 'A button'
+                        }                    
                 ]
             }
         ]
